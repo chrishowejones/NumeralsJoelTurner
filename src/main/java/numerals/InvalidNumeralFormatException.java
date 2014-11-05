5729c9771f53f2fcb535a0feffd6f849c60ad737
Joel Turner <joel@joels-air.home>
2014-11-05 22:03:54 +0000

    Added numerals to number method....

---
+++ src/main/java/numerals/InvalidNumeralFormatException.java
@@ -0,0 +1,10 @@
+package numerals;
+
+public class InvalidNumeralFormatException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/main/java/numerals/Numerals.java
+++ src/main/java/numerals/Numerals.java
@@ -1,24 +1,29 @@
 package numerals;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * Created by chrishowe-jones on 03/11/2014.
  */
 public class Numerals {
+	
+	private static int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	private static String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	private static Map<String, Integer> numeralsMap = new HashMap<String, Integer>();
+	
+	static {
+        for(int i = 0; i < numbers.length; i++){
+        	numeralsMap.put(numerals[i], numbers[i]);
+        }
+	}
+	
     public static String getNumeral(int i) {
-
-        int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder sb = new StringBuilder();
         while(i != 0){
             int index = 0;
             for(int number : numbers){
-
                 if(i >= number){
                     sb.append(numerals[index]);
                     i = i - number;
@@ -27,11 +32,31 @@ public class Numerals {
                 index++;
             }
         }
-
-
-
-
         return sb.toString();
-
     }
+
+	public static int getNumber(String string) throws InvalidNumeralFormatException {	
+		
+		if (!string.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")){
+			throw new InvalidNumeralFormatException();
+		}
+		
+		char[] numeralArray = string.toCharArray();
+		int total = 0;
+		
+		for (int i = 0; i < numeralArray.length; i++){
+			
+			Integer currentNumeralValue = numeralsMap.get(String.valueOf(numeralArray[i]));
+			Integer nextNumeralValue = (i < numeralArray.length - 1) ? numeralsMap.get(String.valueOf(numeralArray[i+1])) : 0;
+			
+			if (i == numeralArray.length - 1 || currentNumeralValue >= nextNumeralValue){
+				total = total + currentNumeralValue;
+			} else if (currentNumeralValue < nextNumeralValue){
+				total = total + nextNumeralValue - currentNumeralValue;
+				i++;
+			}
+		}
+		return total;
+	}
+
 }
